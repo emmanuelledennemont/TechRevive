fa5e726f51a414aa70dc6f979dc19ec7498b0662
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-21 16:14:57 +0400

    Initial Commit

---
+++ TechRvive/TechRviveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechRviveApp.swift
+//  TechRvive
+//
+//  Created by Emmanuelle  Dennemont on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TechRviveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
