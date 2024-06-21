5c9ce4048eaba436ad2ff1b8daae03c698a8d023
Ezequiel Gomes <ezequiel7897@gmail.com>
2024-06-21 14:27:56 +0200

    PremierCommitEzequiel

--- TechRvive/ContentView.swift
+++ TechRvive/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, La Team")
+            Text("Hellooooooo")
         }
         .padding()
     }
